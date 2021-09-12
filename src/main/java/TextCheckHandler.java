f36fbca711d120bef292d31ff2bf088cb4a01adc
Velore <1327313677@qq.com>
2021-09-12 20:34:06 +0800

    编写并测试主启动类TextCheckHandler

--- src/main/java/TextCheckHandler.java
+++ src/main/java/TextCheckHandler.java
@@ -1,6 +1,46 @@
+import utils.HashUtils;
+import utils.IOUtils;
+
+/**
+ * @author chenzhuohong
+ */
 public class TextCheckHandler {
 
-    public static void TextCheck(String readPath, String writePath){
+    /**
+     * 获取两字符串的海明距离
+     * @param simHash1 字符串1
+     * @param simHash2 字符串2
+     * @return 海明距离
+     */
+    public static double getHammingDistance(String simHash1, String simHash2){
+        double hammingDistance = 0;
+        for(int i = 0; i<128 ; i++){
+            //对比simhash的每一位
+            if(simHash1.charAt(i)!=simHash2.charAt(i)){
+                hammingDistance++;
+            }
+        }
+        return hammingDistance;
+    }
 
+    /**
+     * 主启动类
+     * @param originPath 初始文本路径
+     * @param comparePath 对比文本路径
+     * @param answerPath 写入结果的路径
+     */
+    public static void textCheck(String originPath, String comparePath, String answerPath){
+        //计算初始文本的simhash
+        String originSimHash = HashUtils.getSimHash(IOUtils.read(originPath));
+        //计算对比文本的simhash
+        String compareSimHash = HashUtils.getSimHash(IOUtils.read(comparePath));
+        //计算两文本重复率
+        double repetitiveRate =(128 - getHammingDistance(originSimHash, compareSimHash))/128;
+        //将重复率写入结果路径
+        IOUtils.write(Double.toString(repetitiveRate), answerPath);
+    }
+
+    public static void main(String[] args) {
+        textCheck(args[0], args[1], args[2]);
     }
 }
--- src/main/java/utils/HashUtils.java
+++ src/main/java/utils/HashUtils.java
@@ -11,6 +11,16 @@ import java.util.List;
  */
 public class HashUtils {
 
+    /**
+     * 二进制hash的长度
+     */
+    private static final int HASH_BIN_LENGTH = 128;
+
+    /**
+     * 将字节数组转换为二进制字符串
+     * @param bytes 字节数组
+     * @return 二进制字符串
+     */
     private static String bytesToBinString(byte[] bytes) {
         StringBuilder builder = new StringBuilder();
         for (byte b : bytes) {
@@ -31,10 +41,14 @@ public class HashUtils {
     public static String getHash(String word){
         StringBuilder wordHash = null;
         try{
+            //通过md5算法获取字符串的摘要信息
             MessageDigest md = MessageDigest.getInstance("MD5");
+            //将字符串的摘要信息更新至MessageDigest
             md.update(word.getBytes());
+            //获取字符串的二进制hash并转换为字符串
             wordHash = new StringBuilder(bytesToBinString(md.digest()));
-            int a = 128 - wordHash.length();
+            int a = HASH_BIN_LENGTH - wordHash.length();
+            //hash长度不足128位，在末尾补0
             if(a > 0){
                 for(int i = 0 ; i < a ; i++){
                     wordHash.append("0");
@@ -54,19 +68,19 @@ public class HashUtils {
     public static String getSimHash(String textStr){
         List<Term> wordList = StandardTokenizer.segment(textStr);
         //128位的加权向量表
-        int[] weightedList = new int[128];
+        int[] weightedList = new int[HASH_BIN_LENGTH];
         for (Term term : wordList) {
             if(term.nature != Nature.w){
                 //wordHash每个分出的词都计算出128位哈希值
                 String wordHash = HashUtils.getHash(term.word);
-                for (int j = 0; j < 128; j++) {
+                for (int j = 0; j < HASH_BIN_LENGTH; j++) {
                     //哈希值的每一位都与1比较，若等于1则乘以1，否则乘以-1，最后加在加权向量表上
                     weightedList[j] += (wordHash.charAt(j) == '1' ? 1 : -1);
                 }
             }
         }
         StringBuilder builder1 = new StringBuilder();
-        for(int i = 0 ; i < 128 ; i++){
+        for(int i = 0 ; i < HASH_BIN_LENGTH ; i++){
             builder1.append((weightedList[i] > 0 ? 1:0));
         }
         return builder1.toString();
